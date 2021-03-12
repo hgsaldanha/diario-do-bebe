4a3e9b9a6bb8c74945d4d87b2eb772f20d833d08
Hernã Saldanha <hernags@mpf.mp.br>
2021-03-12 11:06:57 -0300

    Commit inicial

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,60 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { Button } from "@chakra-ui/button";
+import { FormErrorMessage } from "@chakra-ui/form-control";
+import { FormLabel } from "@chakra-ui/form-control";
+import { FormControl } from "@chakra-ui/form-control";
+import { Input } from "@chakra-ui/input";
+import { Heading } from "@chakra-ui/layout";
+import { SimpleGrid } from "@chakra-ui/layout";
+import { Container } from "@chakra-ui/layout";
+import { Select } from "@chakra-ui/select";
+import { Field, Form, Formik } from "formik";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    <Container>
+      <Formik
+        initialValues={{ evento: 1, inicio: "", fim: "" }}
+        onSubmit={(values, actions) => {
+          console.log(actions);
+        }}
+      >
+        {(props) => (
+          <Form>
+            <SimpleGrid>
+              <Heading as="h1" mb={2}>Novo evento</Heading>
+              <Field component={Select} name="evento">
+                <option value={1}>Amamentação</option>
+                <option value={2}>Sono</option>
+              </Field>
+              <Field name="inicio">
+                {({ field, form }) => (
+                  <FormControl
+                    isInvalid={form.errors.inicio && form.touched.inicio}
+                  >
+                    <FormLabel htmlFor="inicio">Início</FormLabel>
+                    <Input {...field} id="inicio" placeholder="Início" />
+                    <FormErrorMessage>{form.errors.inicio}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              <Field name="fim">
+                {({ field, form }) => (
+                  <FormControl isInvalid={form.errors.fim && form.touched.fim}>
+                    <FormLabel htmlFor="fim">Fim</FormLabel>
+                    <Input {...field} id="fim" placeholder="Fim" />
+                    <FormErrorMessage>{form.errors.fim}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              <Button type="submit" mt={4} size="lg" colorScheme="blue">
+                Salvar
+              </Button>
+            </SimpleGrid>
+          </Form>
+        )}
+      </Formik>
+    </Container>
+  );
 }
